47f1f6cc77766e194e4579c6c13658950235ebeb
Ehan Hasainar <ehanhasainar@gmail.com>
2025-04-19 18:06:08 +0400

    Update password-generator.py
    Formatted the spaces

--- password-generator.py
+++ password-generator.py
@@ -4,9 +4,13 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
+print()
 zero = int(input("How many letters would you like in your password? ")) 
+print()
 one = int(input(f"How many symbols would you like? "))
-two = int(input(f"How many numbers would you like? "))
+print()
+two = int(input(f"How many numbers would you like?"))
+print()
 
 password = str()
 
